5f89b11a8ee7813da60a0e0f3ee953b0500d0845
Fata Nugraha <fatanugraha@outlook.com>
2017-11-17 04:32:06 +0700

    add cors

--- twiggsy_api/settings.py
+++ twiggsy_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'nowi4mt-fx&e5()-yxarsqz(_q-2%ydnvw&l$!p^$$tu*&$d-9'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ORIGIN_ALLOW_ALL=True
 
 # Application definition
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
     'user',
     'campaign',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
