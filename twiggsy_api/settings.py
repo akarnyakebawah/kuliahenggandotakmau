5b0ba28d2cd94c185af19d117ba60fcff0109bce
Degoldie Sonny <degoldiesonny98@gmail.com>
2017-11-17 00:20:44 +0700

    fixed whitenoise config

--- twiggsy_api/settings.py
+++ twiggsy_api/settings.py
@@ -146,5 +146,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
