eb68bcf1f9a79995c69ad4af1931994d82511440
satraul <satraul@gmail.com>
2018-01-31 14:17:09 +0700

    temporary hid picture field

--- user/models.py
+++ user/models.py
@@ -62,7 +62,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_('active'), default=True)
     is_staff = models.BooleanField(_('is staff'), default=False)
     birth_date = models.DateField(blank=True, null=True)
-    picture = models.ImageField(upload_to=picture_directory_path, default="users/picture/anonymous.jpg")
+    #picture = models.ImageField(upload_to=picture_directory_path, default="users/picture/anonymous.jpg")
     gender = models.CharField(_('gender'), choices=GENDER_CHOICES, max_length=16, default="other")
 
     objects = UserManager()
